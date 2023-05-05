be7bfc9acc0c7e8a10d1ffd20828880b96217615
valenezka <valentina.barro18@gmail.com>
2023-05-05 18:33:53 -0300

    animar actualizado

--- proyecto/imagenes_movil.py
+++ proyecto/imagenes_movil.py
@@ -1,16 +1,6 @@
-import itertools
+
 import pygame
 import os
-class AnimatedBackground(pygame.sprite.Sprite):
-    def __init__(self, position, images, delay):
-        super(AnimatedBackground, self).__init__()
-
-        self.images = itertools.cycle(images)
-        self.image = next(self.images)
-        self.rect = pygame.Rect(position,  self.image.get_rect().size)
-
-        self.animation_time = delay
-        self.current_time = 0
 
 def Animar():
     
@@ -20,14 +10,13 @@ def Animar():
     size = (800, 335)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Ultima Carrera del Rayo")
-    #Fondo de pantalla
-
-    imagen = None
+    
+   
     dir = sorted(os.listdir("choque"))
-    #images =  [pygame.image.load("choque" + os.sep + file_name).convert() for file_name in sorted(os.listdir("choque"))]
+    
     # Configuración de la variable de juego que indica si se está ejecutando o no
     running = True
-    # Loop principal del juegoç
+    # Loop principal del juego
     i=0
     while running:
         # Manejo de eventos
@@ -43,3 +32,5 @@ def Animar():
         if (i == dir.__len__()-1):
             break
         i+=1
+
+Animar()
